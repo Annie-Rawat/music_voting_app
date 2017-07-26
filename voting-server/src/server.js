54e9cc7272025083ad41c397e964e518cd987454
Roboneet <neethumariyajoy@gmail.com>
2017-07-26 21:15:20 +0530

    broadcasting changes to all the connections

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,5 +1,13 @@
 import Server from 'socket.io'
 
-export default function startServer(){
+export default function startServer(store){
 	var io = new Server().attach(3000);
+
+	store.subscibe({
+		() => io.emit('state', store.getState().toJS())
+	})
+
+	io.on('connection', (socket) => {
+		socket.emit('state', store.getState().toJS())
+	})
 }
